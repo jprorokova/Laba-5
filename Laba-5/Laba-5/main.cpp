02fb235ea9510ffaa1ce07eda03b48f0e28284c5
Julia <j.prorokova@gmail.com>
2020-08-06 12:33:29 +0300

    created main.cpp

---
+++ Laba-5/Laba-5/main.cpp
@@ -0,0 +1,19 @@
+﻿
+#include <iostream>
+using namespace std;
+
+
+int main(int argc,char* argv[])
+{
+    string db = argv[0];
+    setlocale(0, "");
+    if (argc != 4) {
+        cout << "Check parametres!" << endl;
+    }
+  
+    double latitude = atof(argv[1]);
+    double longitude = atof(argv[2]);
+    long int size = atof(argv[3]);
+
+}
+
